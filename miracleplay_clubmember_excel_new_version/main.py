df83065fb1cd8ad2d2ec9eaf43bf57c1bfd32753
yangjaehoon <34858808+yangjaehoon@users.noreply.github.com>
2024-12-30 15:24:28 +0900

    11월 버전

--- miracleplay_clubmember_excel_new_version/main.py
+++ miracleplay_clubmember_excel_new_version/main.py
@@ -13,10 +13,11 @@ service = Service(ChromeDriverManager().install())
 
 # 드라이버 생성 및 페이지 접근
 with webdriver.Chrome(service=service) as driver:
+    driver.get('https://polygon.miracleplay.gg/club/detail/132?year=2024&month=10')  # 폴리곤
     #driver.get('https://base.miracleplay.gg/club/detail/135?year=2024&month=10')  # 베이스
     #driver.get('https://opbnb.miracleplay.gg/club/detail/133?year=2024&month=10')  # 오피비엔비
     #driver.get('https://arbirum.miracleplay.gg/club/detail/189?year=2024&month=10')  # 아비트럼
-    driver.get('https://xpla.miracleplay.gg/club/detail/201?year=2024&month=10')  # 엑스플라
+    #driver.get('https://xpla.miracleplay.gg/club/detail/201?year=2024&month=10')  # 엑스플라
 
 
     # 버튼이 있으면 계속 클릭
@@ -48,10 +49,11 @@ with webdriver.Chrome(service=service) as driver:
     df = pd.DataFrame(table_data, columns=['rank', 'name', 'cp', 'contribution'])
 
     # 현재 작업 디렉토리에 엑셀 파일로 저장
+    file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_polygon.xlsx" #폴리곤
     #file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_base.xlsx" #베이스
     #file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_opbnb.xlsx" #오피비엔비
     #file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_arbitrum.xlsx" #아비트럼
-    file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_xpla.xlsx" #엑스플라
+    #file_path = "C:/Users/lljhm/Desktop/miracleplay_whale club/whale_club_cp_xpla.xlsx" #엑스플라
 
     df.to_excel(file_path, index=False)
 
